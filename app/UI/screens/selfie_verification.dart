f07aca9db1876f5b17ce35b1db3997c6323d8645
Abdul Qayoum Danish <friendflow.app@gmail.com>
2025-07-17 08:16:37 -0700

    Add screen Dart files to UI/screens folder
    Uploaded the following screen files:
    - home_screen.dart
    - profile_screen.dart
    - vip_screen.dart
    - chat_screen.dart
    - selfe_verification.dart

---
+++ app/UI/screens/selfie_verification.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfieVerification extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Selfie Verification Process')),
+    );
+  }
+}
